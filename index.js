e140ddcd67889137b5128eab468b3833d14c3a77
Angela Perez <angela.perez@flatironschool.com>
2018-08-04 01:38:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function scuberGreetingForFeet(distance) {
   if (distance <= 400) {
     console.log("this one is on me!");
   }
-  else if (distance > 2000) {
+  else if (distance > 2000 && distance < 2500) {
   console.log('I will gladly take your thirty bucks.');
   }
   else if (distance > 2500) {
