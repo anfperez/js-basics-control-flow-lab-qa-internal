22de58bf559f8870473f50d6dd70c2e91401324e
Angela Perez <angela.perez@flatironschool.com>
2018-08-04 01:39:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,15 @@ function scuberGreetingForFeet(distance) {
   }
 }
 */
-
+function scuberGreetingForFeet (feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (1999 < feet && feet < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
+}
 function ternaryCheckCity(city) {
   return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
