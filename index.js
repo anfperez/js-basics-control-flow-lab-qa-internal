7490294df24b566f4b4dacd8c7ded6994ecd0bf0
Angela Perez <angela.perez@flatironschool.com>
2018-08-04 01:50:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your code in this file!
+/*
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    console.log("this one is on me!");
+  }
+  else if (distance > 2000 && distance < 2500) {
+  console.log('I will gladly take your thirty bucks.');
+  }
+  else if (distance > 2500) {
+    console.log('No can do.');
+  }
+}
+*/
+function scuberGreetingForFeet (feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet > 1999 && feet < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
+}
+
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+  case "generous":
+    return 'Thank you so much.'
+    break;
+  case "not as generous":
+    return 'Thank you.'
+    break;
+  default:
+    return "Bye.";
+}
+}
