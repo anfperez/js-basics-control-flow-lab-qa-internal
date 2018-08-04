333a2a6abdc5902740ca5ca1107d8c60363b1699
Angela Perez <angela.perez@flatironschool.com>
2018-08-04 01:30:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 function scuberGreetingForFeet(distance) {
-  if (distance >= 400) {
+  if (distance <= 400) {
     console.log("this one is on me!");
   }
   else if (distance )
